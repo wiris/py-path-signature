83b6a5ded80b4e9f33ee1fba1bed5842661c8d29
Jaume Gibert <jaume.gibert@wiris.com>
2021-09-23 06:17:56 +0000

    test: Add tests for path signature result

--- tests/test_units.py
+++ tests/test_units.py
@@ -1,12 +1,10 @@
+import json
+import os
+
+import numpy as np
 import pytest
 from py_path_signature.data_models.stroke import Stroke
 from py_path_signature.path_signature_extractor import PathSignatureExtractor
-
-
-@pytest.fixture(scope="class")
-def path_signature_extractor():
-    path_signature_extractor = PathSignatureExtractor()
-    yield path_signature_extractor
 
 
 @pytest.mark.parametrize(
@@ -37,30 +35,41 @@ def test_bounding_box(input_strokes, expected_bounding_box):
     assert bounding_box == expected_bounding_box
 
 
-@pytest.mark.parametrize(
-    "input_strokes, expected_bounding_box",
-    [
-        (
-            [{"x": [1, 2, 3], "y": [1, 2, 3]}],
-            (1, 1, 2, 2),
-        ),
-        (
-            [{"x": [0, 1, 2, 3], "y": [1, 2, 3, 4]}, {"x": [6, 8, 2, 3], "y": [0, 2, 3, 9]}],
-            (0, 0, 9, 8),
-        ),
-        (
-            [
-                {"x": [714, 1], "y": [3, 4]},
-                {"x": [6, 8], "y": [0, 9]},
-                {"x": [100, 8], "y": [10, 9]},
-            ],
-            (0, 1, 10, 713),
-        ),
-    ],
-)
-def test_bounding_box(path_signature_extractor, input_strokes, expected_bounding_box):
+def list_test_cases():
+    test_cases_dir = "tests/data/strokes"
+    return [
+        os.path.splitext(case)[0]
+        for case in os.listdir(test_cases_dir)
+        if os.path.isfile(os.path.join(test_cases_dir, case))
+    ]
+
+
+@pytest.fixture(scope="function", params=list_test_cases())
+def strokes_and_reference_signature(request):
+
+    test_case = request.param
+
+    with open(f"tests/data/strokes/{test_case}.txt") as f:
+        strokes = json.load(f)
+
+    path_signature = np.load(f"tests/data/signatures/{test_case}.npy")
+
+    return (strokes, path_signature)
+
+
+@pytest.fixture(scope="class")
+def path_signature_extractor():
+    path_signature_extractor = PathSignatureExtractor(
+        order=2, rendering_size=(128, -1), min_rendering_dimension=5, max_aspect_ratio=30, delta=5
+    )
+    return path_signature_extractor
+
+
+def test_image_signatures(path_signature_extractor, strokes_and_reference_signature):
+
+    input_strokes, path_signature_groundtruth = strokes_and_reference_signature
 
     strokes = [Stroke(**stroke) for stroke in input_strokes]
-    bounding_box = path_signature_extractor.calculate_bounding_box(strokes=strokes)
+    path_signature = path_signature_extractor.extract_signature(strokes=strokes)
 
-    assert bounding_box == expected_bounding_box
+    assert (path_signature == path_signature_groundtruth).all()
